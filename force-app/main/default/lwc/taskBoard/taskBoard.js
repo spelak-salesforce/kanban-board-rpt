45af31ffbad68a5aa258d3e6310b02f56edf65a6
spelak-salesforce <spelak@salesforce.com>
2019-02-13 16:29:07 -0500

    User Story 3

--- force-app/main/default/lwc/taskBoard/taskBoard.js
+++ force-app/main/default/lwc/taskBoard/taskBoard.js
@@ -1,41 +1,83 @@
-import { LightningElement, api, wire, track } from 'lwc';
+import { LightningElement, api, wire } from 'lwc';
 import getTasks from '@salesforce/apex/TaskBoardController.getTasks';
+import updateTaskStatus from '@salesforce/apex/TaskBoardController.updateTaskStatus';
+import { refreshApex } from '@salesforce/apex';
+import { ShowToastEvent } from 'lightning/platformShowToastEvent';
 
 export default class TaskBoard extends LightningElement {
     //Allows the component to be aware of the ID of the record (i.e. the Account)
     //that it is embedded on
     @api recordId;
 
-    @track notStartedTasks = [];
-    @track inProgressTasks = [];
-    @track completedTasks = [];
-    @track waitingOnSomeoneTasks = [];
-    @track deferredTasks = [];
-    
     @wire(getTasks, { recordId: '$recordId' })
-    processTasks({ error, data }) {
-        //Seems repetitive, is there a better way to do this?
-        if (data) {
-            data.forEach(element => {
-                if (element.Status === 'Not Started') {
-                    this.notStartedTasks.push(element);
-                }
-                else if (element.Status === 'In Progress') {
-                    this.inProgressTasks.push(element);
-                }
-                else if (element.Status === 'Completed') {
-                    this.completedTasks.push(element);
-                }
-                else if (element.Status === 'Waiting on someone else') {
-                    this.waitingOnSomeoneTasks.push(element);
-                }
-                else if (element.Status === 'Deferred') {
-                    this.deferredTasks.push(element);
-                }
+    tasks;
+
+    get notStartedTasks() {
+        return Array.isArray(this.tasks.data) ? this.tasks.data.filter(task => task.Status === "Not Started") : [];
+    }
+    get inProgressTasks() {
+        return Array.isArray(this.tasks.data) ? this.tasks.data.filter(task => task.Status === "In Progress") : [];
+    }
+    get completedTasks() {
+        return Array.isArray(this.tasks.data) ? this.tasks.data.filter(task => task.Status === "Completed") : [];
+    }
+    get waitingOnSomeoneTasks() {
+        return Array.isArray(this.tasks.data) ? this.tasks.data.filter(task => task.Status === "Waiting on someone else") : [];
+    }
+    get deferredTasks() {
+        return Array.isArray(this.tasks.data) ? this.tasks.data.filter(task => task.Status === "Deferred") : [];
+    }
+
+    startTaskCardDrag(event) {
+        event.dataTransfer.setData("text/plain", event.target.dataset.taskId);
+        console.log(
+            'startTaskCardDrag', 
+            'Task ID', event.target.dataset.taskId, 
+            'id', event.target.id
+        );
+    }
+
+    dragTaskCardOver(event) {
+        event.preventDefault();
+    }
+
+    dropTaskCard(event) {
+        console.log(
+            'dropTaskCard', 
+            event, 
+            'id', 
+        );
+
+        updateTaskStatus({
+            taskId: event.dataTransfer.getData("text/plain"), 
+            status: event.currentTarget.dataset.taskStatus
+        })
+            .then(() => {
+                // Requery tasks
+                refreshApex(this.tasks)
+                    .then(() => {
+                        this.dispatchEvent(new ShowToastEvent({
+                            title: "Success",
+                            message: "Task Status changed",
+                            variant: "success"
+                        }));
+                    })
+                    .catch(error => {
+                        this.dispatchEvent(new ShowToastEvent({
+                            title: "Error",
+                            message: error,
+                            variant: "error"
+                        }));
+                    });
+            })
+            .catch(error => {
+                this.dispatchEvent(new ShowToastEvent({
+                    title: "Error",
+                    message: error,
+                    variant: "error"
+                }));
             });
-            this.record = data;
-        } else if (error) {
-            console.log(error);
-        }
+
+        event.preventDefault();
     }
 }
