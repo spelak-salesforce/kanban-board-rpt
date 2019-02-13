e4a480703d92834ff8b2dd50c183e54980b965da
spelak-salesforce <spelak@salesforce.com>
2019-02-13 12:59:20 -0500

    User Story 2

--- force-app/main/default/lwc/taskCard/taskCard.js
+++ force-app/main/default/lwc/taskCard/taskCard.js
@@ -1,23 +1,73 @@
-import { LightningElement, api } from 'lwc';
+import { LightningElement, api, track } from 'lwc';
 import saveTasks from '@salesforce/apex/TaskBoardController.saveTasks';
 
 export default class TaskCard extends LightningElement {
-    //NOTE: @api properties are immutable
+    // NOTE: @api properties are immutable
     @api task
+
+    @track isEditing = false;
+    @track error
+
+    description
+
+    changeDescription(event) {
+        this.description = event.target.value;
+    }
 
     edit() {
         console.log('enable editing here!');
+        
+        // Prepare for editing
+        {
+            // Reset description to task's Description for init or if there was a canceled edit
+            this.description = this.task.Description;
+        }
+
+        // Start editing
+        {
+            this.isEditing = true;
+        }
     }
 
     //Basic wiring to a save method in Salesforce server
     save() {
+        console.log('Clicked save');
+
         let taskToSave = Object.assign({}, this.task);
-        saveTasks({ tasks: [taskToSave] })
-            .then(() => {
-                console.log('Success!');
+        {
+            taskToSave.Description = this.description;
+
+            console.log(
+                'description', this.description, 
+                'taskToSave.Description', taskToSave.Description,     
+                'taskToSave', taskToSave
+            );
+        }
+        saveTasks({tasks: [taskToSave]})
+            .then(result => {
+                console.log('Success!', result);
+
+                // Set task as result
+                {
+                    this.task = result[0];
+                }
+                
+                // Stop editing
+                {
+                    this.isEditing = false;
+                }
             })
-            .catch(() => {
-                console.log('Something went wrong...');
+            .catch(error => {
+                console.log('Something went wrong...', error);
             })
+            
+        console.log('tried to save tasks');
+    }
+
+    cancel() {
+        // Stop editing
+        {
+            this.isEditing = false;
+        }
     }
 }
